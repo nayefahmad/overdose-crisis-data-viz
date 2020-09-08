c22d05f35d27234f7d659bbc9ab76ceeb1ac0088
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2020-09-07 19:04:05 -0700

    add data_long_w2 as drake target

---
+++ src/functions.R
@@ -0,0 +1,88 @@
+
+#' Get cleaned data 
+#'
+#' @param data Raw dataframe, from file ScopingReviewOfOpioi-FinalResults_DATA_2020-07-06_1942.csv 
+#'
+#' @return Cleaned dataframe (before converting to long format) 
+#' 
+get_clean_data <- function(data){
+    data <- 
+        data %>% mutate(year_pub = ifelse(year_pub %in% c("Unclear", "unclear"), NA, year_pub))
+    
+    data <- 
+        data %>% mutate(paper_type = ifelse(record_id %in% c("Mallatt_2019_2", "Sears_2019"), 1, paper_type), 
+                            outcomes_yn = ifelse(record_id == "Minhee_2019", 0, outcomes_yn))
+    
+    # renaming the over the counter variable - for some reason redcap coding it as an impact variable
+    data <- data %>% 
+        rename(outcome_otc___1 = impact_non_pres_avail_2___1, 
+               outcome_otc___2 = impact_non_pres_avail_2___2, 
+               outcomes_non_pres_other = outcome_non_pres_other, 
+               outcomes_non_pres_other_2 = outcome_non_pres_other_2, 
+               outcomes_non_pres_other_3 = outcome_non_pres_other_3, 
+               outcomes_prescription_other = outcomes_pres_other, 
+               outcomes_prescription_other_2 = outcomes_pres_other_2, 
+               outcomes_prescription_other_3 = outcomes_pres_other_3)
+    
+    data_n <- data %>% mutate_at(11:411, as.character)  # todo: why not factor? 
+    sapply(data_n, class)
+    
+    # rename variables ----
+    # will need this later for pivot_longer function
+    test <- data_n %>% 
+        select(-longitudinal_yn) %>%
+        rename_at(vars(ends_with("_other")), 
+                  funs(str_replace(., "_other", "___9090"))) %>% 
+        rename_at(vars(ends_with("_other_2")), 
+                  funs(str_replace(., "_other", "___9090"))) %>% 
+        rename_at(vars(ends_with("_other_3")), 
+                  funs(str_replace(., "_other", "___9090"))) %>% 
+        select(1:10, 50:53, 103, 193, 409:410, everything()) %>% 
+        rename("population_specific___9090" = population___9090, 
+               "population_specific_affect___9090" = population___9090_2)
+    
+    return(test)
+}
+
+
+get_data_long <- function(test){
+    data_long <- test %>% 
+        pivot_longer(cols = country_income___1:impact_other_3___999,  # everything after the first 18 cols 
+                     names_to = c("variables", "code"),
+                     names_pattern = "(.*)___(.*)",  
+                     values_to = "out") %>% 
+        filter(out != 0)
+    return(data_long)
+}
+
+
+get_data_long_code <- function(data_long, lookup_a){
+    data_long_code <- 
+        data_long %>% 
+        mutate(variable_merge = ifelse(str_detect(variables, pattern = "impact_"), 
+                                       "impact",
+                                       variables)) %>% 
+        left_join(lookup_a, by = c("variable_merge", "code")) %>% 
+        mutate(code_name = ifelse(code %in% c("9090", "9090_2", "9090_3"), 
+                                  out,
+                                  code_name))
+    return(data_long_code)
+    
+}
+
+
+get_data_long_w2 <- function(data_long_code){
+    data_long_w2 <- data_long_code %>% 
+        select(-c(variable_merge, out)) %>% 
+        filter(code != "999") %>%
+        unite(code_comb, c(code, code_name)) %>%
+        group_by(record_id, variables) %>%
+        arrange(record_id) %>%
+        mutate(row = row_number()) %>%
+        pivot_wider(names_from = variables, values_from = code_comb) %>%
+        select(-row)
+    
+    return(data_long_w2)
+}
+
+
--- src/make.R
+++ src/make.R
@@ -1,11 +1,21 @@
-library(drake)
 
+library(tidyverse)
+library(drake)
+library(here)
+
+source(here("src", "functions.R"))
 
 plan <- drake_plan(
     raw_data = read_csv(file_in("data/ScopingReviewOfOpioi-FinalResults_DATA_2020-07-06_1942.csv")), 
     lookup = read_csv(file = file_in("data/lookup_rd.csv")), 
-    clean_data = raw_data %>% mutate(year_pub = 
-                                        ifelse(year_pub %in% c("Unclear", "unclear"), NA, year_pub))
+    lookup_a = lookup %>% select(-impact_var), 
+    lookup_b = lookup %>% rename(out_op = variable_merge) %>% 
+        unite(out_specific, c(code, code_name), remove = FALSE), 
+    clean_data = raw_data %>% get_clean_data(),
+    data_long = get_data_long(clean_data),
+    data_long_code = get_data_long_code(data_long, lookup_a), 
+    data_long_w2 = get_data_long_w2(data_long_code)
 )
 
+make(plan)
 vis_drake_graph(plan)
