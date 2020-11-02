bb6b1fa73a436d2cd589e53b25c2cc4394f0c173
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2020-11-01 20:33:16 -0800

    add regions filter

--- src/app.r
+++ src/app.r
@@ -14,14 +14,26 @@ library(lubridate)
 library(shiny)
 library(shinyWidgets)
 library(DT)
+library(RCurl)
+library(stringr)
 
 # set up data ---- 
-df1_all_data <- 
-  read_csv(here("dst", "05_join_pop_mod_cor_file.csv"))
+x <- getURL("https://raw.githubusercontent.com/nayefahmad/overdose-crisis-data-viz/master/dst/05_join_pop_mod_cor_file.csv") 
+df1_all_data <- read.csv(text = x)
 
 cols <- colnames(df1_all_data)
 
-continent <- df1_all_data
+country_regions <- 
+  df1_all_data %>% 
+  pull(country_region) %>%
+  as.character() %>% 
+  unique()
+
+country_regions <- 
+  country_regions[(!is.na(country_regions))]  %>% 
+  unname()
+country_regions2 <- sapply(country_regions, function(x){substr(x, 3, nchar(x))})  
+
 
 
 # __________----
@@ -30,7 +42,7 @@ continent <- df1_all_data
 ui <- fluidPage(
   title = "Overdose crisis",
   
-  h1(class = "page-header", "Overdose crisis visualization", tags$br(), tags$small("subtitle")),
+  h1(class = "page-header", "Overdose crisis visualization", tags$br(), tags$small("todo:subtitle")),
   
   navbarPage(
     title = "Pages",
@@ -38,14 +50,16 @@ ui <- fluidPage(
     # > 2.1) Panel 1 ----------
     
     tabPanel(
-      title = "Tables",
+      title = "Data summary",
       value = "page_1",  # page identifier
       
       # >> Header ----
       p(class = "lead",
-        "lorem ipsum", 
-        "lorem ipsum"
+        "Lead text ", 
+        "Lead text"
       ), 
+      
+      p("more text more text"), 
       
       tags$br(), 
       
@@ -58,9 +72,9 @@ ui <- fluidPage(
           wellPanel(
             # WUC selection
             pickerInput(
-              inputId = "input_demo", 
-              label   = "Selection",
-              choices = c("Option 1", "Option 2"),
+              inputId = "input_country_region", 
+              label   = "Select country region",
+              choices = country_regions,
               multiple = FALSE, 
               options = pickerOptions(
                 actionsBox = FALSE,
@@ -73,16 +87,23 @@ ui <- fluidPage(
               inputId = "input_cols", 
               label = "Select columns", 
               choices = cols, 
-              selected = c("record_id", "policy_type"), 
+              selected = c("record_id", "country_regions", "policy_type"), 
               multiple = TRUE,
               options = NULL)
           )
         ), 
         
         # >> Plot outputs -----
+        
+        column(
+          width = 9, 
+          h4("Summaries"), 
+          div(plotOutput(outputId = "out_plot1"))
+        ), 
+        
         column(
           width = 9,
-          h4("Data"), 
+          h4("Full dataset"), 
           div(dataTableOutput(outputId = "out_table")) 
         ) 
       ) 
@@ -102,8 +123,16 @@ server <- function(input, output, session){
   df2_filtered <- 
     reactive(
       df1_all_data %>% 
-        select(input$input_cols)
+        filter(country_region == input$input_country_region) %>% 
+        select(input$input_cols) 
+        
     ) 
+  
+  output$out_plot1 <- renderPlot(
+    mtcars %>% 
+      ggplot(aes(x = mpg, y = disp)) + 
+      geom_point()
+  )
   
   
   output$out_table <- renderDataTable(
