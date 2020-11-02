b8da310fcc1c840a6336436105aa3cbc3de54ce3
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2020-11-01 21:15:07 -0800

    add placeholder text

--- src/app.r
+++ src/app.r
@@ -50,16 +50,19 @@ ui <- fluidPage(
     # > 2.1) Panel 1 ----------
     
     tabPanel(
-      title = "Data summary",
+      title = "Full datset",
       value = "page_1",  # page identifier
       
       # >> Header ----
       p(class = "lead",
-        "Lead text ", 
-        "Lead text"
+        "Use the filters below to explore the full dataset. "
       ), 
       
-      p("more text more text"), 
+      p("The criminalisation of people who use drugs compounds drug-related challenges and worsens health and welfare outcomes. Across the world, there is a growing number of jurisdictions where the possession of scheduled drugs for personal use has been decriminalised. In some countries and federal states, this has been extended to the cultivation of cannabis for personal use or the sharing of substances where there is no financial gain (also known as ‘social sharing’)."), 
+
+      p("In the context of drug policy, ‘decriminalisation’ refers to the removal of criminal penalties for activities related to drug use. In some legal systems criminal penalties are replaced by civil sanctions, while in other systems no penalties are applied – please see our glossary for more detailed definitions."), 
+
+      p("Some form of decriminalisation has been adopted in 30 countries – with significant differences and levels of effectiveness. This interactive map provides an overview of these models, offering insights into decriminalisation laws, their implementation and impact."), 
       
       tags$br(), 
       
@@ -70,12 +73,14 @@ ui <- fluidPage(
           
           # >> WellPanel 1 --------
           wellPanel(
-            # WUC selection
+            
+            # Country region selection
             pickerInput(
               inputId = "input_country_region", 
               label   = "Select country region",
               choices = country_regions,
-              multiple = FALSE, 
+              multiple = TRUE, 
+              selected = c("5_North America"), 
               options = pickerOptions(
                 actionsBox = FALSE,
                 liveSearch = TRUE,
@@ -83,22 +88,22 @@ ui <- fluidPage(
               )
             ), 
             
+            # Column selection 
             selectizeInput(
               inputId = "input_cols", 
               label = "Select columns", 
               choices = cols, 
-              selected = c("record_id", "country_regions", "policy_type"), 
+              selected = c("record_id", "country_region", "policy_type"), 
               multiple = TRUE,
               options = NULL)
           )
-        ), 
+        )
+      ), 
         
         # >> Plot outputs -----
-        
+      div(  
         column(
-          width = 9, 
-          h4("Summaries"), 
-          div(plotOutput(outputId = "out_plot1"))
+          width = 3
         ), 
         
         column(
@@ -109,9 +114,31 @@ ui <- fluidPage(
       ) 
     ), 
     
+    # > 2.2) Panel 2  ----------
+    tabPanel(
+      title = "Highlights", 
+      value = "page_2", 
+      
+      div( 
+        column(
+          width = 9, 
+          h4("Summaries"), 
+          div(plotOutput(outputId = "out_plot1"))
+        )
+      ) 
+    ), 
+    
+    # > 2.3) Panel 3 ----------
     tabPanel(
       title = "Background",
-      value = "page_2"
+      value = "page_2", 
+      
+      p("The criminalisation of people who use drugs compounds drug-related challenges and worsens health and welfare outcomes. Across the world, there is a growing number of jurisdictions where the possession of scheduled drugs for personal use has been decriminalised. In some countries and federal states, this has been extended to the cultivation of cannabis for personal use or the sharing of substances where there is no financial gain (also known as ‘social sharing’)."), 
+      
+      p("In the context of drug policy, ‘decriminalisation’ refers to the removal of criminal penalties for activities related to drug use. In some legal systems criminal penalties are replaced by civil sanctions, while in other systems no penalties are applied – please see our glossary for more detailed definitions."), 
+      
+      p("Some form of decriminalisation has been adopted in 30 countries – with significant differences and levels of effectiveness. This interactive map provides an overview of these models, offering insights into decriminalisation laws, their implementation and impact.") 
+      
     ) 
   ) 
 )
@@ -131,7 +158,8 @@ server <- function(input, output, session){
   output$out_plot1 <- renderPlot(
     mtcars %>% 
       ggplot(aes(x = mpg, y = disp)) + 
-      geom_point()
+      geom_point() + 
+      labs(title = "todo: this is just a placeholder")
   )
   
   
